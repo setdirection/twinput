6c0fd62803bfa424bc7e96dfd02ee9a8d1257db0
Dion Almaer <dion@almaer.com>
2010-12-29 10:27:50 -0800

    add more settings

--- plugin/src/twinput.js
+++ plugin/src/twinput.js
@@ -15,11 +15,16 @@
   $.fn.twinput = function(opts) {
 
     var defaults = {
-      autoShrinkURLs : true, // take a long URL and shrink it *if space is needed*
-      autoShrinkWords: true, // make changes to the input (e.g. s/and/\&/) *if space is needed*
-      autoLookupNames: true, // if user types '@d' lookup friends (case insensitively!) in dropdown based on username AND real name (e.g. dalmaer and Dion Almaer)
-      allowUpload    : true, // have the upload button that then shows a file upload for users
-      useLocation    : false // default setting for sending geo location
+      onlyShrinkWhenNeeded : true,  // if there is plenty of space, don't shrink the URLs or words
+      autoShrinkURLs       : true,  // take a long URL and shrink it *if space is needed*
+      autoShrinkWords      : true,  // make changes to the input (e.g. s/and/\&/) *if space is needed*
+      autoLookupNames      : true,  // if user types '@d' lookup friends (case insensitively!) in dropdown based on username AND real name (e.g. dalmaer and Dion Almaer)
+      allowUpload          : true,  // have the upload button that then shows a file upload for users
+      useLocation          : false, // default setting for sending geo location
+      tweetWithLocation    : false, // add another button to tweet with the location
+      useKeyboardShortcuts : false, // allow keyboard shortcuts for actions
+      useCharacterPalette  : true,  // allow the palette action to popup a palette of chars
+      expandTypingArea     : false  // the input is made larger so you can see everything
     };
     
     // If options are passed in, merge them with the defaults (options win)
