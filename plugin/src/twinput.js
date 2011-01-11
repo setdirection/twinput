b6a985e90905e1980539f4c2bbd4f66b25c79a66
ewpreston <ericw.preston@gmail.com>
2011-01-11 09:25:21 -0600

    Added function to shrink URLs using bit.ly; made it manual with a button for now; needs OAuth instead of user-based auth; needs to happen auto

--- plugin/src/twinput.js
+++ plugin/src/twinput.js
@@ -15,6 +15,7 @@
 (function($) {
 
   var MAX_CHARS = 140;
+  var NUM_NERVOUS_CHARS = 140; // Number of remaining chars to start getting nervous about reaching max length
   
   var getInputColorGradient = function(n) {
     var green = parseInt(251.0*n, 10);
@@ -53,18 +54,68 @@
     elem.css("color", getCountColorGradient(remain));
     
     textarea.css("background-color", getInputColorGradient(remain/140.0));
-    if (len > 0 && len < (MAX_CHARS+1)) {
+    if (btn && (len > 0) && len < (MAX_CHARS+1)) {
       btn.removeAttr('DISABLED');
-    } else {
+    } else if (btn) {
       btn.attr('DISABLED', true);
     }
+    return remain;
+  };
+  
+  var shortenUrl = function(url, callback) {
+    //TODO use OAuth here instead; this is just for testing
+    var user = "ewpreston";
+    var key = "R_edcd67a19bc686f2b667e39e07cf1fc1";
+    
+    $.getJSON("http://api.bit.ly/v3/shorten?callback=?",
+              {
+                login: user,
+                apiKey: key,
+                longUrl: url,
+                format: "json"
+              },
+              function(data) { callback(data.data.url, url); });
+    
+    //TODO how to return the shortened URL?
+  };
+  
+  var replaceLongUrl = function(textarea, shortUrl, longUrl, countElem) {
+    //alert("replaceLongUrl " + shortUrl + " long " + longUrl);
+    var text = textarea.val();
+    console.log("replaceLongUrl long: " + longUrl + " short: " + shortUrl);
+    if (shortUrl && longUrl) {
+      textarea.val(text.replace(longUrl, shortUrl));
+    }
+    
+    updateInput(textarea, countElem);
   };
   
   var updateInput = function(textarea, countElem, btn) {
-    updateCount(textarea, countElem, btn);
+    var remain = updateCount(textarea, countElem, btn);
     
     // Is this too expensive to do every keypress event?
-    sessionStorage.savedTweet = textarea.val();
+    localStorage.savedTweet = textarea.val();
+    return remain;
+  };
+  
+  // Shrink URLs, common words, etc.
+  var shrinkTweet = function(textarea, countElem) {
+    var result;
+    var text = textarea.val();
+    // Stolen from Crockford
+    // var parse_url = /(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?/g;
+    // Stolen from http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+    var parse_url = /\b((?:[a-z][\w]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?]))/gmi;
+    
+    var callback = function(shortUrl, longUrl){ replaceLongUrl(textarea, shortUrl, longUrl, countElem); };
+    
+    while ((result = parse_url.exec(text))) {
+      if (result) {
+        shortenUrl(result[0], callback);
+      }
+    }
+    
+    updateInput(textarea, countElem);
   };
   
   var sendTweet = function(val) {
@@ -81,7 +132,7 @@
   };
   
   var reloadTweet = function(textarea, countElem, btn) {
-    var savedTweet = sessionStorage.savedTweet;
+    var savedTweet = localStorage.savedTweet;
     if (savedTweet) {
       textarea.val(savedTweet);
       updateCount(textarea, countElem, btn);
@@ -101,14 +152,14 @@
       useCharacterPalette  : true,  // allow the palette action to popup a palette of chars
       expandTypingArea     : false  // the input is made larger so you can see everything
     };
-    
+
     // If options are passed in, merge them with the defaults (options win)
     if (opts) {
       $.extend(defaults, opts);
     }
 
     // Add buttons and counter after textarea div
-    $('<div style="float: right;"><span id="charcount" class="tweet-counter">140</span><button id="cleartweet">Clear</button><button id="reloadtweet">Reload</button><button id="tweetbtn">Tweet</button></div>').appendTo(".twinputdiv");
+    $('<div style="float: right;"><span id="charcount" class="tweet-counter">140</span><button id="shrinktweet">Shrink</button><button id="cleartweet">Clear</button><button id="reloadtweet">Reload</button><button id="tweetbtn">Tweet</button></div>').appendTo(".twinputdiv");
     
     // This runs for all of the matches
     return this.each(function() {  
@@ -139,9 +190,13 @@
         reloadTweet(textarea, $("#charcount"), tweetBtn);
       });
 
+      $("#shrinktweet").click(function() {
+        shrinkTweet(textarea, $("#charcount"));
+      });
+
     });
     
   };
   
-})(jQuery);
+}(jQuery));
 
